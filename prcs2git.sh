957220db1b9c99981558b1594bbb243ff6e945e1
TANIGUCHI Takaki <takaki@asis.media-as.org>
2009-02-27 11:49:27 +0900

    add copyright notice

--- prcs2git.sh
+++ prcs2git.sh
@@ -1,18 +1,40 @@
 #!/bin/bash 
+# prcs2git - convert PRCS repository to Git repository
+# Copyright (C) 2009 TANIGUCHI, Takaki <takaki@asis.media-as.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 set -e
 
-if [ $# -ne 1 ]; then
-	echo Usage: $(basename $0) package
-	exit 1
-fi
-
 package=$1
-
-basedir=/tmp/p2g
+basedir=/tmp/prcs2git
 pdir=${basedir}/prcs/${package}
 gdir=${basedir}/git/${package}
 edir=${basedir}/export/${package}
+
+if [ $# -ne 1 ]; then
+	cat <<EOF
+Usage: $(basename $0) package
+
+New Git repository placed at ${edir}package.
+
+prcs2git - Copyright (C) 2009 TANIGUCHI Takaki <takaki@asis.media-as.org>
+This program comes with ABSOLUTELY NO WARRANTY.  This is free software,
+and you are welcome to redistribute it under certain conditions
+EOF
+	exit 1
+fi
 
 revs=($(prcs info --sort=date ${package} | grep -v '\*DELETED\*' | awk '{print $2}'))
 

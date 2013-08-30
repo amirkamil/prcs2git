a4d3f364e9711487fb9a1bb3893fe31ebf768bac
Amir Kamil <kamil@cs.berkeley.edu>
2013-08-29 17:41:37 -0700

    Modify script to handle one revision at a time. Remove assumption that a 0 branch exists. Add ability to specify an email server for converting author information.

--- prcs2git.sh
+++ prcs2git.sh
@@ -18,15 +18,20 @@
 set -e
 
 package=$1
+server=$2
 basedir=/tmp/prcs2git
 pdir=${basedir}/prcs/${package}
 gdir=${basedir}/git/${package}
+ddir=${basedir}/done/${package}
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 1 ]; then
 	cat <<EOF
-Usage: $(basename $0) project
+Usage: $(basename $0) project [email_server]
 
 New Git repository is exported at ${gdir}project.
+
+If email server is provided, then a prcs user is converted to a git author
+as user@email_server.
 
 prcs2git - Copyright (C) 2009 TANIGUCHI Takaki <takaki@asis.media-as.org>
 This program comes with ABSOLUTELY NO WARRANTY.  This is free software,
@@ -35,38 +40,49 @@ EOF
 	exit 1
 fi
 
-revs=($(prcs info --sort=date ${package} | grep -v '\*DELETED\*' | awk '{print $2}'))
+revs=($(prcs info --sort=date ${package} | grep ${package} | grep -v '\*DELETED\*' | awk '{print $2}'))
 
 branches=($(for i in ${revs[@]}; do 
 	echo $i | sed -e 's/\.[0-9]\+$//'
 done | sort -u ))
 
+mkdir -p ${ddir}
+if [ ! -d ${gdir} ]; then
+    mkdir -p ${gdir}
+    cd ${gdir}
+    git init
+fi
+
+first=1
+
 for i in ${revs[@]}; do 
-    mkdir -p ${pdir}/${i}
-    cd ${pdir}/${i}
-    if [ ! -f ${package}.prj ]; then
+	if [ -f ${ddir}/${i} ]; then
+	    continue
+	fi
+
+	mkdir -p ${pdir}/${i}
+	cd ${pdir}/${i}
 	prcs checkout -f -r${i} ${package} 
-    fi
-done
+	cd ${gdir}
 
-rm -rf  ${gdir}
-mkdir -p ${gdir}
-cd ${gdir}
-git init
-
-for i in ${revs[@]}; do 
 	c_info=$(cd /; prcs info -f -r${i} -l ${package})
 	c_branch=$(echo $i | sed -e 's/\.[0-9]\+$//')
 	c_branch_rev=$(echo $i | sed -e 's/^.*\.//')
 	p_revs=($(echo "${c_info}" | grep Parent-Version: | awk '{print $2}' ))
 
-	date=$(echo "${c_info}" | cut -d' ' -f 3-8)
+	c_info_lite=$(cd /; prcs info -r${i} ${package} | tr -d '\n')
+	date=$(echo "${c_info_lite}" | cut -d' ' -f 3-8)
+	auth=$(echo "${c_info_lite}" | cut -d' ' -f 10)
+	full_auth="${auth} <${auth}@${server}>"
 
-	if [ "${i}" = "0.1" ] ;then
-	    rsync --exclude=.git --delete -ac "${pdir}/0.1/." .
+	if [ ${first} = 1 ]; then
+	    rsync --exclude=.git --delete -ac "${pdir}/${i}/." .
 	    git add .
 	    git commit -a --date="${date}" -m "${c_info}"
-	    git branch -m master 0
+	    git branch -m master ${c_branch} 
+	    touch ${ddir}/${i}
+	    rm -rf ${pdir}/${i}
+	    first=0
 	    continue
 	fi
 	
@@ -89,7 +105,14 @@ for i in ${revs[@]}; do
 
 	git add -u 
 	git add . 
-	git commit -a --date="${date}" -m "${c_info}" 
+	if [ -n "${server}" ]; then
+	    git commit -a --date="${date}" --author="${full_auth}" -m "${c_info}"
+	else
+	    git commit -a --date="${date}" -m "${c_info}"
+	fi
+
+	touch ${ddir}/${i}
+	rm -rf ${pdir}/${i}
 done
 
 rm -rf ${pdir}
